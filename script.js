0df8715b51b53fa410d8f7b3b4c98bb9f5682273
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2023-02-25 13:13:41 +0000

    add console log

--- script.js
+++ script.js
@@ -12,5 +12,6 @@ document.querySelector('.format').addEventListener('click', () => {
     const chineseWord = format_data(document.querySelector('.chinese').value)
     const englishWord = format_data(document.querySelector('.english').value)
     const result = format_into_cards(englishWord, chineseWord)
+    console.log(result)
     document.querySelector('.result').textContent = result
 })
